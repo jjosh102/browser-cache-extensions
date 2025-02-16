22963b75382ecd6941c85f5b83fde19994ba9dbf
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-02-16 10:38:56 +0800

    Refactor LocalCacheItem to use init-only properties for immutability and simplify expiration logic

--- src/LocalStorage/LocalCacheItem.cs
+++ src/LocalStorage/LocalCacheItem.cs
@@ -1,19 +1,13 @@
 namespace BrowserCache.Extensions.LocalStorage;
 public class LocalCacheItem<T>(T? data, TimeSpan? timeToLive = null)
 {
-    private DateTime _created = DateTime.UtcNow;
-
     public T? Data { get; init; } = data;
 
-    public TimeSpan? TimeToLive { get; } = timeToLive;
+    public TimeSpan? TimeToLive { get; init; } = timeToLive;
 
-    public DateTime Created
-    {
-        get => _created;
-        set => _created = value;
-    }
+    public DateTime Created { get; init; } = DateTime.UtcNow;
 
-    public DateTime? ExpiresAt => TimeToLive.HasValue ? _created.Add(TimeToLive.Value) : null;
+    public DateTime? ExpiresAt => TimeToLive.HasValue ? Created.Add(TimeToLive.Value) : null;
 
     public bool IsExpired() => ExpiresAt is { } exp && DateTime.UtcNow >= exp;
 }
